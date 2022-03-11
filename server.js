d2d3df55aeabf9b945fa51f8a0aee72b255e61dc
Leonardorossato <leonardorossato21@gmail.com>
2022-03-11 15:29:43 -0300

    rotas removidas, pois n estavam sendo utilizadas

--- server.js
+++ server.js
@@ -3,21 +3,16 @@ const app = express();
 const bodyParser = require('body-parser');
 require('dotenv').config()
 const port = process.env.PORT
-const userRouter = require('./routes/userRouter')
-const carsRouter = require('./routes/carsRouter')
-const sequelize = require('./connection/sequelize')
+const sequelize = require('./connection/sqliteConnection')
 
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
 sequelize.sync().then(()=>{
     console.log('Connection with Sqlite is established')
-}).catc(err =>{
+}).catch(err =>{
     console.log('Error connecting with Sqlite: ' + err)
 })
-
-app.use('/api/users', userRouter)
-app.use('/api/cars', carsRouter)
 
 app.listen(port, ()=>{
     console.log(`Listening the port : ${port}`)
