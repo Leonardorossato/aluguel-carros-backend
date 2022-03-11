8aad37931c7715e8355a9fcbc0a3d4e388ad8ca9
Leonardorossato <leonardorossato21@gmail.com>
2022-03-11 14:48:20 -0300

    model attendent criado

---
+++ models/Attendant.js
@@ -0,0 +1,40 @@
+const sequelize = require('../connection/sqliteConnection')
+const {Model, DataTypes} = require('sequelize')
+
+class Attendant extends Model {}
+
+Attendant.init({
+    id : {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    matricula:{
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    ctps:{
+        type: DataTypes.NUMBER,
+        allowNull: false
+    }
+}, {
+    sequelize,
+    tableName: 'attendants',
+    timestamps: true
+})
+
+module.exports = Attendant
