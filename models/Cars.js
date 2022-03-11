face5d28cc6a2333d94bd962fa5003cafa041a21
Leonardorossato <leonardorossato21@gmail.com>
2022-03-11 14:49:16 -0300

    novo movel cars em ingles para sequelize

---
+++ models/Cars.js
@@ -0,0 +1,45 @@
+const sequelize = require('../connection/sqliteConnection')
+const {Model, DataTypes} = require('sequelize')
+
+class Cars extends Model {}
+
+Cars.init({
+    id : {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true
+    },
+    placa:{
+        type: DataTypes.NUMBER,
+        allowNull: false,
+        unique: true
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    year: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    type: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: 'false'
+    },
+    fuel:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    color:{
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    sequelize,
+    tableName: 'cars',
+    timestamps: true
+})
+
+module.exports = Cars
