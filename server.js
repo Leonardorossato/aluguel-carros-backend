980b359ec07143738e1a29c992c74abc4c165a89
Leonardorossato <leonardorossato21@gmail.com>
2022-03-11 14:26:46 -0300

    ajsute nos imports a rota client para users

--- server.js
+++ server.js
@@ -3,16 +3,21 @@ const app = express();
 const bodyParser = require('body-parser');
 require('dotenv').config()
 const port = process.env.PORT
-const clientRouter = require('./routes/clientRouter')
-const carRouter = require('./routes/carRouter')
+const userRouter = require('./routes/userRouter')
+const carsRouter = require('./routes/carsRouter')
 const sequelize = require('./connection/sequelize')
 
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
+sequelize.sync().then(()=>{
+    console.log('Connection with Sqlite is established')
+}).catc(err =>{
+    console.log('Error connecting with Sqlite: ' + err)
+})
 
-app.use('/api/clients', clientRouter)
-app.use('/api/carros', carRouter)
+app.use('/api/users', userRouter)
+app.use('/api/cars', carsRouter)
 
 app.listen(port, ()=>{
     console.log(`Listening the port : ${port}`)
